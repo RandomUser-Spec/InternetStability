80cd5b15633b5557baef1f4631aaba996cf14d04
RandomUser-Spec <76138622+RandomUser-Spec@users.noreply.github.com>
2022-06-09 19:31:44 -0500

    Add files via upload

---
+++ CPUCONSUMPTION.py
@@ -0,0 +1,29 @@
+import matplotlib .pyplot as plt
+import psutil as p
+app_name_dict = {}
+count = 0
+for process in p.process_iter():
+    count = count + 1
+    if count <= 6:
+        getApplication = process.name()
+        cpu_usage = p.cpu_percent()
+        print('name : ', getApplication , '-- cpu_usage : ', cpu_usage)
+        app_name_dict.update({getApplication : cpu_usage})
+keymax= max(app_name_dict, key = app_name_dict.get)
+print(keymax)
+keymin = min(app_name_dict, key = app_name_dict.get)
+print(keymin)
+name_list = [keymax, keymin]
+print(name_list)
+app = app_name_dict.values()
+max_app = max(app)
+print(max_app)
+min_app= min(app)
+print(min_app)
+max_min_list = [max_app,min_app]
+print(max_min_list)
+plt.figure(figsize = (15,7))
+plt.xlabel("Maximum - Minumum")
+plt.ylabel("Usage")
+plt.bar(name_list, max_min_list,width = 0.6, color = ("purple","green","red","blue","orange","pink"))
+plt.show()
